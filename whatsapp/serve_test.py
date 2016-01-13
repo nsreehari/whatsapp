ca14627ecb6c8b1d7b253bae9d6407fcc3a2e658
Sanjay Prakash <sanjaya.g.prakash1992@gmail.com>
2016-01-13 15:17:05 +0000

    serve test

---
+++ whatsapp/serve_test.py
@@ -0,0 +1,31 @@
+#!/opt/bitnami/python/bin/python
+#-*- coding: utf-8 -*-
+
+
+from copy import deepcopy
+import json
+from serve import *
+
+
+serve = Serve()
+
+from os.path import isfile
+while True:
+    mymsg = raw_input("=> ") 
+    jsondict = {
+      "msgtype": "text", "msgbody": mymsg, "medium": "myapp", "phonenum": "919700000000"
+    }
+    receivejson = json.dumps(jsondict)
+    if receivejson != None:
+        resp = serve.getResponse(jsondict)
+        if resp:
+            if resp['restype'] == 'list':
+                respc = deepcopy(resp)
+                for (typ, val) in resp['response']:
+                   respc['restype'] = typ
+                   respc['response'] = val
+                   print respc
+            else:
+               print resp
+    else:
+        time.sleep(2.6)
